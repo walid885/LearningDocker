9d93b46150b18ffcb3e3aaa69d4dddb7cd465944
walid885 <bettaieb88walid@gmail.com>
2023-03-14 16:35:48 +0100

    problms in the connection , to be solved

---
+++ DockerComposeApp/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from redis import Redis 
+app = Flask(__name__)
+redis = Redis(host='Redis',port=6377)
+@app.route('/')
+def hello():
+	count = redis.incr('hits')
+	return 'Hello world , I have been seen {} times. \n'.format(count)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
+
